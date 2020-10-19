f89d888a1ef09319fc53da9b75cc6e2038cc903f
Song ki ho <44698603+GodVas@users.noreply.github.com>
2020-10-20 00:57:58 +0900

    Update TanCalculation.php

--- src/skh6075/MagicSpell/calculation/calculations/TanCalculation.php
+++ src/skh6075/MagicSpell/calculation/calculations/TanCalculation.php
@@ -16,12 +16,18 @@ class TanCalculation extends Calculation{
     
     public function calculation (Vector3 &$vec, int $slot, float $number1): void{
         $keys = array_keys (self::CALCULATION_SLOT) [$slot];
-        if ($keys === 'x') {
-            $vec->x = $vec->x + tan ($number1);
-        } else if ($keys === 'y') {
-            $vec->y = $vec->y + tan ($number1);
-        } else if ($keys === 'z') {
-            $vec->z = $vec->z + tan ($number1);
+        switch ($keys) {
+            case 'x':
+                $vec->x = $vec->x + tan ($number1);
+                break;
+            case 'y':
+                $vec->y = $vec->y + tan ($number1);
+                break;
+            case 'z':
+                $vec->z = $vec->z + tan ($number1);
+                break;
+            defualt:
+                break;
         }
     }
 }
