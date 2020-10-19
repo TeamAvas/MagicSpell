a272c42e4d289ef8377d91f801ca5d0c2efea94a
Song ki ho <44698603+GodVas@users.noreply.github.com>
2020-10-20 00:51:51 +0900

    Update DivideCalculation.php

--- src/skh6075/MagicSpell/calculation/calculations/DivideCalculation.php
+++ src/skh6075/MagicSpell/calculation/calculations/DivideCalculation.php
@@ -16,12 +16,18 @@ class DivideCalculation extends Calculation{
     
     public function calculation (Vector3 &$vec, int $slot, float $number1): void{
         $keys = array_keys (self::CALCULATION_SLOT) [$slot];
-        if ($keys === 'x') {
-            $vec->x = $vec->x / $number1;
-        } else if ($keys === 'y') {
-            $vec->y = $vec->y / $number1;
-        } else if ($keys === 'z') {
-            $vec->z = $vec->z / $number1;
+        switch ($keys) {
+            case 'x':
+                $vec->x = $vec->x / $number1;
+                break;
+            case 'y':
+                $vec->y = $vec->y / $number1;
+                break;
+            case 'z':
+                $vec->z = $vec->z / $number1;
+                break;
+            default:
+                break;
         }
     }
 }
