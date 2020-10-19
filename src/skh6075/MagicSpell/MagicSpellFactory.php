109f1ff2bb35d216e904e9777006ad7fcb1707d7
Song ki ho <44698603+GodVas@users.noreply.github.com>
2020-10-20 01:03:30 +0900

    Update MagicSpellFactory.php

--- src/skh6075/MagicSpell/MagicSpellFactory.php
+++ src/skh6075/MagicSpell/MagicSpellFactory.php
@@ -8,15 +8,28 @@ use skh6075\MagicSpell\magicspell\MagicSpell;
 
 class MagicSpellFactory{
 
+    /** @var MagicSpellFactory */
+    private static $instance;
+    
     /** @var MagicSpell[] */
     private static $spells = [];
     
+    
+    public static function getInstance (): MagicSpellFactory{
+        if (self::$instance === null) {
+            self::$instance = new self ();
+        }
+        returns self::$instance;
+    }
+    
+    private function __construct () {
+    }
     
     /**
      * @param string $name
      * @return MagicSpell|null
      */
-    public static function getMagicSpell (string $name): ?MagicSpell{
+    public function getMagicSpell (string $name): ?MagicSpell{
         return self::$spells [$name] ?? null;
     }
     
@@ -24,7 +37,7 @@ class MagicSpellFactory{
      * @param string $name
      * @param array $data
      */
-    public static function registerMagicSpell (string $name, array $data): void{
+    public function registerMagicSpell (string $name, array $data): void{
         if (!isset ($data ["events"]) || !isset ($data ["entities"]) || !isset ($data ["particle"])) {
             throw new MagicSpellRegisterException ("Could not find data value.");
         }
@@ -34,20 +47,20 @@ class MagicSpellFactory{
     /**
      * @param string $name
      */
-    public static function unregisterMagicSpell (string $name): void{
+    public function unregisterMagicSpell (string $name): void{
         if (isset (self::$spells [$name])) {
             unset (self::$spells [$name]);
         }
     }
     
-    public static function init (): void{
+    public function init (): void{
         foreach (array_diff (scandir (MagicSpellLoader::getInstance ()->getDataFolder ()), [ '.', '..' ]) as $value) {
             if (is_dir (MagicSpellLoader::getInstance ()->getDataFolder () . $value)) {
                 continue;
             }
             if (isset (explode ('.', $value) [1]) and explode ('.', $value) [1] === "json") {
                 try {
-                    self::registerMagicSpell ($value, json_decode (file_get_contents (MagicSpellLoader::getInstance ()->getDataFolder () . $value), true));
+                    $this->registerMagicSpell ($value, json_decode (file_get_contents (MagicSpellLoader::getInstance ()->getDataFolder () . $value), true));
                 } catch (MagicSpellRegisterException $exception) {
                     MagicSpellLoader::getInstance ()->getLogger ()->critical ($exception->getMessage ());
                 }
